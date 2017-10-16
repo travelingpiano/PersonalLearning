86a244812de05c0c9d5418c73181875369ecfd07
travelingpiano <lihsuanlu@gmail.com>
2017-10-15 21:21:26 -0700

    having classes and interfaces in typescript

--- Angular4/Typescript/main.ts
+++ Angular4/Typescript/main.ts
@@ -1,2 +1,18 @@
 let message = 'abc';
-let endsWithC = message.endsWith('c');
+// let endsWithC = message.endsWith('c');
+
+class Point {
+  x: number;
+  y: number;
+
+  draw() {
+    console.log('x is ' + this.x + ' and y is ' + this.y);
+  }
+
+  getDistance(another: Point) {
+
+  }
+}
+
+let point = new Point();
+point.draw();
