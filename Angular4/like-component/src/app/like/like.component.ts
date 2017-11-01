c8dccace1f64eba88768d297e23dde85e54b3815
travelingpiano <lihsuanlu@gmail.com>
2017-10-31 22:00:26 -0700

    practiced inputs with like component

--- Angular4/like-component/src/app/like/like.component.ts
+++ Angular4/like-component/src/app/like/like.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-like',
@@ -7,12 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikeComponent implements OnInit {
 
-  likeStatus: boolean;
-  likeCount: number;
+  @Input() likeStatus: boolean;
+  @Input() likeCount: number;
 
   constructor() {
-    this.likeStatus = false;
-    this.likeCount = 0;
   }
 
   ngOnInit() {
